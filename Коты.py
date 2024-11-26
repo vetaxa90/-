8f4bbac3ab897c5ad2d752c77ce1000b6d33fef2
vetaxa90 <efimovich3@gmail.com>
2024-11-26 19:49:18 +0200

    В место кнопки добавлено меню

--- Коты.py
+++ Коты.py
@@ -24,6 +24,8 @@ def set_image():
         label.config(image=img)
         label.image = img
 
+def exit():
+    window.destroy()
 
 window =Tk()
 window.title("Cats")
@@ -32,9 +34,16 @@ window.geometry("600x620")
 
 label= Label()
 label.pack()
-update_button =Button(text= "Обновить",command = set_image)
-update_button.pack()
+# update_button =Button(text= "Обновить",command = set_image)
+# update_button.pack()
+meny_bar= Menu(window)
+window.config(menu=meny_bar)
 
+file_menu =Menu(meny_bar,tearoff=0)
+meny_bar.add.cascade(label="Файл",menu=file_menu)
+file_menu.add_command(label="Загрузить фото",command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label="Выход",command=exit)
 url = "https://cataas.com/cat"
 set_image()
 
