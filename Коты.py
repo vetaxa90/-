0b5c08ba8c1df44d34590d81064c18a325af16f3
vetaxa90 <efimovich3@gmail.com>
2024-11-26 19:06:38 +0200

    Создано окно виджеты

--- Коты.py
+++ Коты.py
@@ -1,1 +1,24 @@
 from tkinter import *
+from PIL import Image,ImageTk
+import requests
+from io import BytesIO
+
+from pygame.examples.aliens import load_image
+from клик import window
+
+window =Tk()
+window.title("Cats")
+window.geometry("600x400")
+
+
+label= label()
+label.pack()
+
+
+
+url = "hrttps:// cataas.com/cat"
+img= load_image(url)
+if img:
+    label.config(image=img)
+    label.image = img
+window.mainloop()
