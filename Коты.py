1d0407529bfa595042fe6a70d797ab651f8288c1
vetaxa90 <efimovich3@gmail.com>
2024-11-26 19:37:54 +0200

    Добавили изменение размера

--- Коты.py
+++ Коты.py
@@ -12,6 +12,7 @@ def load_image(url):
         response.raise_for_status()
         image_data =BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600,480),Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e :
         print(f"Произошла ошибка{e}")
@@ -26,7 +27,7 @@ def set_image():
 
 window =Tk()
 window.title("Cats")
-window.geometry("600x400")
+window.geometry("600x620")
 
 
 label= Label()
